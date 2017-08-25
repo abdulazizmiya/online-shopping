2ff5da4706d6c1ffdd1a0e30730f43a55385c696
Abdul Aziz Miya <abdulazizmiyame@gmail.com>
2017-08-25 11:17:31 -0700

    succfully loaded satatic resources

---
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -0,0 +1,59 @@
+package net.kzn.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+
+public class PageController {
+	
+	
+	
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index(){
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","welcme here");
+		return mv;
+	}
+	
+/*	@RequestMapping(value="/test")
+public ModelAndView test(@RequestParam(value="greeting",required=false)String greeting){
+		
+		if(greeting==null){
+			greeting="hello there";
+			
+		}
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting","welcme here");
+		return mv; */
+		
+		
+		
+//	}
+	
+	
+	
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting){
+			
+			if(greeting==null){
+				greeting="hello there";
+				
+			}
+			ModelAndView mv=new ModelAndView("page");
+			mv.addObject("greeting","welcome here");
+			return mv; 
+	
+	
+	}
+	
+	
+}
+
+
+
